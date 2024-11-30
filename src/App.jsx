691b1ff4d7e1f15ef8aba2521cfa3cf194643b10
João Pinto <58951511+jdpintoGA@users.noreply.github.com>
2024-11-30 21:53:34 +0100

    css added
    failed to add scss/sass, check after

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Hello from "./components/Hello";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Hello />
     </div>
   );
--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,9 +1,14 @@
 import ButtonComp from "./ButtonComp";
+import Potato from "../Img/Potato.svg";
+import "../css/style.css";
 
 function Hello() {
   return (
-    <div>
+    <div id="body">
       <ButtonComp></ButtonComp>
+
+      <img id="potato" src={Potato} alt="potato image" srcset="" />
+
       <h1>so many patatas woooow</h1>
     </div>
   );
